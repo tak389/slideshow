f8023ebbe890101eef20c5f7d36e6796f672fce2
tak389 <pupu8241t@gmail.com>
2024-03-21 02:04:14 +0900

    必要な記述だったので、戻す
    Signed-off-by: tak389 <pupu8241t@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()]
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, 'src'),
+    },
+  },
 })
